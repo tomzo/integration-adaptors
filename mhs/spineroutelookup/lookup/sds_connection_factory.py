1b150c7d106581d1f79c21c35073ae9bb5ff50a4
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2020-01-30 17:57:40 +0000

    [PRMT-923] Workaround ldaps connection failure
    There is some issue in python ldap library causing the 'invalid server
    address' error when using domain name in server URL rather than IP.
    Disabling cert validation allows to use IP for connection and ignore
    cert error.

--- mhs/spineroutelookup/lookup/sds_connection_factory.py
+++ mhs/spineroutelookup/lookup/sds_connection_factory.py
@@ -31,7 +31,7 @@ def build_sds_connection_tls(ldap_address: str, private_key: str, local_cert: st
                                                   ca_certs=ca_certs)
 
     load_tls = ldap3.Tls(local_private_key_file=certificates.private_key_path,
-                         local_certificate_file=certificates.local_cert_path, validate=ssl.CERT_REQUIRED,
+                         local_certificate_file=certificates.local_cert_path, validate=ssl.CERT_NONE,
                          version=ssl.PROTOCOL_TLSv1, ca_certs_file=certificates.ca_certs_path)
 
     server = ldap3.Server(ldap_address, use_ssl=True, tls=load_tls)
